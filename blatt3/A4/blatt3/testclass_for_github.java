f0a0d0560eb2a384b4a1c19cacf9cd8ee127ee68
b0ssuz <buenyamin.sarikaya@gmail.com>
2019-05-03 16:07:00 +0200

    Von eclipse gepusht

--- blatt3/A4/blatt3/testclass_for_github.java
+++ blatt3/A4/blatt3/testclass_for_github.java
@@ -3,5 +3,6 @@ package blatt3;
 public class testclass_for_github {
 	public static void main(String[] argvs) {
 		System.out.println("Diese Klasse ist nur zum Testen von git !");
+		System.out.println("Dieser befehl ist nur zum Testen der push funktion von eclipse");
 	}
 }
