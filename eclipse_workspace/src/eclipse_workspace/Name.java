b3234404c4cb10863b26888e69b81cbfe8811526
b0ssuz <buenyamin.sarikaya@gmail.com>
2019-05-05 13:34:01 +0200

    ...

--- eclipse_workspace/src/eclipse_workspace/Name.java
+++
@@ -1,5 +0,0 @@
-package eclipse_workspace;
-
-public class Name {
-
-}
--- eclipse_workspace/src/eclipse_workspace/aufgabe1.java
+++
@@ -1,9 +0,0 @@
-package eclipse_workspace;
-
-public class aufgabe1 {
-	public static void main() {
-		System.out.print("Wie ist dein Name? ");
-		String name = Input.readString();
-		System.out.println("Hallo " + name + "!");
-	}
-}
