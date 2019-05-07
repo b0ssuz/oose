ecbf62982549ca981b23a9fc5bf8a81c8fb929d1
jakobkretz <jakobkretz@10.148.253.12>
2019-05-07 11:04:37 +0200

    jkhkjh

--- blatt3/A3/blatt3/CD.java
+++ blatt3/A3/blatt3/CD.java
@@ -3,6 +3,6 @@ package blatt3;
 public class CD implements Playable {
 	@Override
 	public void song() {
-		System.out.println("O Tannenbaum..");
+		System.out.println("O Tannenbaum.. O du hast ne Frau gehauen!");
 	}
 }
