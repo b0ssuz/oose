05bab0696d420fb4a5d0e52c4e8d534b38801ae6
jakobkretz <jakobkretz@10.148.253.12>
2019-05-07 10:58:18 +0200

    Samir ist ne Hoe

--- blatt3/A3/blatt3/Kassette.java
+++ blatt3/A3/blatt3/Kassette.java
@@ -4,7 +4,7 @@ public class Kassette implements Playable {
 
 	@Override
 	public void song() {
-		System.out.println("Alle meine Entchen..");
+		System.out.println("Alle meine Entchen.. Samir ist ne hoe");
 	}
 
 }
