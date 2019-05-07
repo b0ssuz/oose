7f2722718f2d6d3e1e250d2ef9dfd8044bf4a6b5
jakobkretz <jakobkretz@10.148.253.12>
2019-05-07 11:15:07 +0200

    verschönert

--- blatt4/Lastwagen.java
+++ blatt4/Lastwagen.java
@@ -1,6 +1,7 @@
-public class Lastwagen extends Auto{
-  int reifen = 8;
-  int getReifen(){
-    return this.reifen;
-  }
+public class Lastwagen extends Auto {
+	int reifen = 8;
+
+	int getReifen() {
+		return this.reifen;
+	}
 }
